a3ce6480eca641306dff8fc720b34cc761dbed98
nivanov344 <72230086+nivanov344@users.noreply.github.com>
2020-12-14 14:41:49 +0200

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,68 @@
+package school;
+
+public class Triangle extends GeometricObject{
+	double side1;
+	double side2;
+	double side3;
+	
+	
+	public Triangle() {
+		side1 = 1;
+		side2 = 1;
+		side3 = 1;
+	}
+	
+	
+	public Triangle(double side1,double side2,double side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+	
+	
+	public double getSide1() {
+		return side1;
+	}
+	
+	
+	public void setSide1(double side1) {
+		this.side1 = side1;
+	}
+	
+	
+	public double getSide2() {
+		return side2;
+	}
+	
+	
+	public void setSide2(double side2) {
+		this.side2 = side2;
+	}
+	
+	
+	public double getSide3() {
+		return side3;
+	}
+	
+	
+	public void setSide3(double side3) {
+		this.side3 = side3;
+	}
+	
+	
+	public double getArea() {
+		double p = (side1 + side2 + side3)/2;
+		return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+	}
+	
+	
+	public double getPerimeter() {
+		return side1 + side2 + side3;
+	}
+	
+	
+	public String printTriangle() {
+		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+	}
+	
+}
